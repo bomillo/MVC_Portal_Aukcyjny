2093aaffe4df7d5eeb16e05fdaf8afbc10a0d546
Dominik Postołowicz <mimik-do@wp.pl>
2022-11-19 18:53:37 +0100

    patterns

---
+++ WebApp/Services/Emails/EmailSenderSaveToDisk.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+
+namespace WebApp.Services.Emails
+{
+    public class EmailSenderSaveToDisk : EmailSenderDecorator
+    {
+        public EmailSenderSaveToDisk(IEmailSender decorated) : base(decorated) { }
+
+        public override void SendMail(MailMessage message)
+        {
+            SmtpClient client = new SmtpClient("smtp");
+            client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+            client.PickupDirectoryLocation = @"mails";
+            client.Send(message);
+            base.SendMail(message);
+        }
+    }
+}
---
+++ WebApp/Services/Emails/EmailService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace WebApp.Services.Emails
+{
+    public interface IEmailSender
+    {
+        public void SendMail(MailMessage message);
+    }
+
+    public class EmailService : IEmailSender
+    {
+        public void SendMail(MailMessage message)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            var basicCredential = new NetworkCredential("dominik.postolowicz@gmail.com", "8EKzNt5wIvUASjR9");
+            smtpClient.EnableSsl = true;
+
+            smtpClient.Host = "smtp-relay.sendinblue.com";
+            smtpClient.Port = 587;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = basicCredential;
+
+            smtpClient.Send(message);
+        }
+    }
+}
