c95fd59e4859f4e6ab16f1190086d2020ba326a3
Dominik Postołowicz <mimik-do@wp.pl>
2022-11-29 23:15:10 +0100

    CHAT

--- WebApp/Controllers/ChatController.cs
+++ WebApp/Controllers/ChatController.cs
@@ -20,309 +20,46 @@ namespace WebApp.Controllers
             _context = context;
         }
 
-        // GET: DirectMessages
-        public async Task<IActionResult> Index()
+        [HttpPost]
+        public async Task<IActionResult> Send(int id, string message)
         {
-            //var portalAukcyjnyContext = _context.DirectMessages.Include(d => d.Receiver).Include(d => d.Sender);
+            var userId = int.Parse(HttpContext.User.Claims.FirstOrDefault(c => c.Type.ToLower().Contains("userid")).Value.ToString().ToString().ToString());
+            _context.DirectMessages.Add(new DirectMessage
+            {
+                Message = message,
+                ReceiverId = id,
+                SenderId = userId,
+                SentTime = DateTime.Now.ToUniversalTime(),
+                Recieved = false
+            });
+            _context.SaveChanges();
+
+            return LocalRedirect($"~/chat?id={id}");
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            var userId = int.Parse(HttpContext.User.Claims.FirstOrDefault(c => c.Type.ToLower().Contains("userid")).Value.ToString().ToString().ToString());
+            var portalAukcyjnyContext = _context.DirectMessages.Include(d => d.Receiver).Include(d => d.Sender).Where(dm => dm.SenderId == userId).ToList().GroupBy(dm => dm.ReceiverId).ToList();
+            var portalAukcyjnyContext2 = _context.DirectMessages.Include(d => d.Receiver).Include(d => d.Sender).Where(dm => dm.ReceiverId == userId).ToList().GroupBy(dm => dm.SenderId).ToList();
+            
+
+
+            portalAukcyjnyContext.AddRange(portalAukcyjnyContext2);
+
             var model = new ChatModel();
 
-            model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 1,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 2,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 2,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 1,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 2,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            }); model.directMessagesHeadlines.Add(new DirectMessage
-            {
-                SenderId = 0,
-                Sender = new User
-                {
-                    Name = "NAME!"
-                },
-                ReceiverId = 1,
-                Receiver = new User
-                {
-                    Name = "NAME 2!"
-                },
-                Message = "Message",
-                SentTime = DateTime.Now
-            });
+            model.directMessagesHeadlines = portalAukcyjnyContext.Select(dms => dms.OrderBy(dm => dm.SentTime).First()).ToList();
+            model.directMessages = portalAukcyjnyContext.First(dms => dms.Any(dm => id == null || dm.ReceiverId == id || dm.SenderId == id)).OrderBy(dm => dm.SentTime).ToList();
+            var firstDm = model.directMessages.First();
 
-            return View(model);
+            model.Id = firstDm.SenderId == userId? firstDm.ReceiverId : firstDm.SenderId;
+            foreach (var dm in model.directMessages) {
+                dm.Recieved = true;
+            }
+
+            _context.SaveChanges();
+            return View("Index",model);
         }
     }
 }
--- WebApp/Models/DTO/ChatModel.cs
+++ WebApp/Models/DTO/ChatModel.cs
@@ -4,11 +4,8 @@ namespace WebApp.Models.DTO
 {
     public class ChatModel
     {
+        public int Id { get; set; }
         public List<DirectMessage> directMessages = new List<DirectMessage>();
         public List<DirectMessage> directMessagesHeadlines = new List<DirectMessage>();
-    }
-
-    public class DirectMessageWithDirectionTag : DirectMessage{
-        
     }
 }
