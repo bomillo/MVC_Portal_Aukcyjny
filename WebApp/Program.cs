3d98a5e81fe04e8c6251f8897000aa0b6377e369
Adam Perkowski <73346398+adpero25@users.noreply.github.com>
2022-12-15 15:28:07 +0100

    added subscription style and fixed functionality

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -32,7 +32,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // BG task services
     builder.Services.AddDbContext<PortalAukcyjnyContext2>(options => 
-                options.UseNpgsql(builder.Configuration.GetConnectionString("PortalAukcyjnyContext")).EnableSensitiveDataLogging());
+                options.UseNpgsql(builder.Configuration.GetConnectionString("PortalAukcyjnyContext")));
     builder.Services.AddHostedService<NBPWorker>();
     builder.Services.AddSingleton<CurrencyDownloadService>();
 
